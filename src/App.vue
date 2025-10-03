91872f0135fb9e6f9960f6e327b3da9da1916500
63829 <63829@gymnasium.ax>
2025-10-03 13:04:45 +0300

    lade till så att todo funkar och man kan ta bort samt snygg numrts

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,12 @@ function addItem() {
   list.value.push(listItem)
   itemText.value = ''
 }
+
+function radera(listItem) {
+  list.value = list.value.filter((obj) => {
+    return obj !== listItem 
+  })
+}
 </script>
 
 <template>
@@ -22,23 +28,58 @@ function addItem() {
     <button v-on:click="addItem">Lägg till</button>
   </header>
   <main>
-    <h3>Att göra</h3>
+    <h3 id="todo">Att göra</h3>
     <ul>
       <template v-for="itm in list" v-bind:key="itm">
-        <li v-if="!itm.done">
-          {{ itm.text }}
+        <li v-if="!itm.done" @click="itm.done=!itm.done" class="todoItm">
+          {{ itm.text }} <span title="Radera" @click="radera(itm)">x</span>
         </li>
       </template>
     </ul>
-    <h3>Klart</h3>
+    <h3 id="klart">Klart</h3>
     <ul>
       <template v-for="itm in list" v-bind:key="itm">
-        <li v-if="itm.done">
-          {{ itm.text }}
+        <li v-if="itm.done" @click="itm.done=!itm.done" class="todoItm done">
+          {{ itm.text }} <span title="Radera" @click="radera(itm)">x</span>
         </li>
       </template>
     </ul>
   </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+  .todoItm {
+    font-weight: bold;
+    padding: 0.5em;
+    list-style-type: none;
+    cursor: pointer;
+
+  }
+  .todoItm:nth-child(odd) {
+    background-color: lightblue;
+  
+  }
+  .todoItm:nth-child(even) {
+    background-color: lightgreen;
+  
+  }
+  .done{
+    text-decoration: line-through solid black;
+  }
+  #klart{
+    color: greenyellow;
+
+  }
+  #todo{
+    color:red;
+  }
+  li span {
+    display: inline-block;
+    position:absolute;
+    right: 3em;
+    background-color: red;
+    color: wheat;
+    padding-left: 0.5em;
+    padding-right: 0.5em;
+  }
+</style>
